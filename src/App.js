323fca316134d22cf421369472b81cbae1e1055f
hishamhk12 <hisham.hk12@gmail.com>
2022-08-21 13:29:57 +0200

    Added my project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect,useState } from 'react';
+import Movie from './Movie';
 
 function App() {
+  const [popular,setPopular]=useState([]);
+  useEffect(()=>{
+    fetchPopular();
+  },[]);
+  // جبنا الداتا
+const fetchPopular = async()=>{
+  const data = await fetch('https://api.themoviedb.org/3/tv/popular?api_key=d3d534a5f12a1c545e4906ff8e35bd6d&language=en-US&page=1')
+
+ //بتحول الداتا بل جايسون
+  const movies = await data.json();
+  setPopular(movies.results);
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className='popular-movie'>
+        {popular.map((movie)=>{
+          return <Movie key={movie.id} movie={movie}/>
+        })}
+        
+      </div>
     </div>
   );
 }
